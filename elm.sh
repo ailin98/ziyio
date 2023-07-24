0c7abc09ce3c5d87ef330006b56f4b604b7757e3
ailin98 <43498502+ailin98@users.noreply.github.com>
2023-07-24 09:25:28 +0800

    Update elm.sh

--- elm.sh
+++ elm.sh
@@ -24,7 +24,7 @@ fi
 
 download_elm(){
 echo "开始下载elm二进制文件到$PWD/ELM目录"
-curl -sS -o $PWD/ELM/elm-$_ftype --create-dirs https://raw.githubusercontent.com/lu0b0/ELM/main/ELM/elm-$_ftype
+curl -sS -o $PWD/ELM/elm-$_ftype --create-dirs https://ghproxy.com/https://raw.githubusercontent.com/lu0b0/ELM/main/ELM/elm-$_ftype
 echo "下载完成，如需重新下载或更新请先删除该文件"
 if [ -f "$PWD/ELM/elm-$_ftype" ]; then
     echo "$PWD/ELM/elm-$_ftype"
@@ -46,7 +46,7 @@ else
         eval "chmod +x $PWD/elm-$_ftype"
         eval "$PWD/elm-$_ftype -t elm"
     else
-        echo "在$PWD/ELM目录、$PWD目录下均未找到文件elm-$_ftype，尝试拉取远程仓库文件elm-$_ftype"
+        echo "在$PWD/ELM目录、$PWD目录下均未找到文件elm-$_ftype,尝试拉取远程仓库文件elm-$_ftype"
         download_elm
     fi
 fi
